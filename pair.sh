b2ded6b8d8ae8356f5ef68d388fef28577324169
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2016-04-14 23:13:35 -0400

    Configure first author with email and name

--- pair.sh
+++ pair.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
+pair_configure() {
+  user1=${1}
+
+  user_response=$(curl "https://api.github.com/users/${1}")
+  email=$(echo "${user_response}" | grep '"email":')
+  email="${email/[ ]*\"email\": \"}"
+  git config pair.author.email "${email/\",}"
+
+  name=$(echo "${user_response}" | grep '"name":')
+  name="${name/[ ]*\"name\": \"}"
+  git config pair.author.name "${name/\",}"
+}
+
 pair() {
   if [ "${1}" == "commit" ]; then
     git $@;
+  else
+    pair_configure $@
   fi
 }
--- spec/pair_spec.rb
+++ spec/pair_spec.rb
@@ -2,15 +2,31 @@ require 'spec_helper'
 
 RSpec.describe 'pair' do
 
-  subject(:command)   { [load_pair, git_stub, 'pair'].join('; ') }
+  subject(:command)   { [load_pair, git_stub, curl_stub, 'pair'].join('; ') }
   let(:load_pair) { '. pair.sh' }
   let(:git_stub) { 'git() { echo "GIT={{$@}}"; }' }
+  let(:github_user_json) { File.read('spec/fixtures/github_user.json') }
+  let(:curl_stub) { "curl() { echo 'CURL={{$@}}'; echo '#{github_user_json}'; }" }
 
-  context 'when pair commits' do
-    subject { `#{command} commit --amend` }
+  describe 'confirure' do
+    context 'when pair is set with just one user' do
+      subject { `#{command} vnegrisolo` }
 
-    it 'call git commit with same params' do
-      is_expected.to include("GIT={{commit --amend}}")
+      it 'call git commit with same params' do
+        is_expected.to include('GIT={{config pair.author.email vinicius.negrisolo@gmail.com}}')
+        is_expected.to include('GIT={{config pair.author.name Vinicius Ferreira Negrisolo}}')
+      end
     end
   end
+
+  describe 'proxy commits' do
+    context 'when pair commits' do
+      subject { `#{command} commit --amend` }
+
+      it 'call git commit with same params' do
+        is_expected.to include('GIT={{commit --amend}}')
+      end
+    end
+  end
+
 end
