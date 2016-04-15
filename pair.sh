99b6328f9baf305b968b4f387ac8a91050739ec2
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2016-04-15 18:45:09 -0400

    Reuse git config set properties for usersx

--- pair.sh
+++ pair.sh
@@ -11,18 +11,28 @@ pair_status() {
   pair_table_line "Committer" "`git config --get pair.committer.name`" "`git config --get pair.committer.email`"
 }
 
+pair_set() {
+  type=${1}
+  email=${2}
+  name=${3}
+
+  git config ${type}.email "${email}"
+  git config ${type}.name "${name}"
+}
+
 pair_configure() {
   type=${1}
   user=${2}
 
   response=$(curl "https://api.github.com/users/${user}")
+
   email=$(echo "${response}" | grep '"email":')
   email="${email/[ ]*\"email\": \"}"
-  git config pair.${type}.email "${email/\",}"
 
   name=$(echo "${response}" | grep '"name":')
   name="${name/[ ]*\"name\": \"}"
-  git config pair.${type}.name "${name/\",}"
+
+  pair_set "${type}" "${email/\",}" "${name/\",}"
 
   if [ -z "${name}" ] || [ -z "${email}" ]; then
     echo "ERROR => You need to set Name and Email for your ${user} on Github"
@@ -31,10 +41,8 @@ pair_configure() {
 }
 
 pair_reset() {
-  git config pair.author.name ""
-  git config pair.author.email ""
-  git config pair.committer.name ""
-  git config pair.committer.email ""
+  pair_set "pair.author"
+  pair_set "pair.committer"
 }
 
 pair_commit() {
@@ -44,18 +52,15 @@ pair_commit() {
   committer_email="`git config --get pair.committer.email`"
 
   if [ -n "${author_email}" ]; then
-    git config user.name "${author_name}"
-    git config user.email "${author_email}"
+    pair_set "user" "${author_email}" "${author_name}"
   fi
 
   git_author=""
   if [ -n "${committer_email}" ]; then
     git_author="--author=\"${committer_name} <${committer_email}>\""
 
-    git config pair.author.name "${committer_name}"
-    git config pair.author.email "${committer_email}"
-    git config pair.committer.name "${author_name}"
-    git config pair.committer.email "${author_email}"
+    pair_set "pair.author" "${committer_email}" "${committer_name}"
+    pair_set "pair.committer" "${author_email}" "${author_name}"
   fi
 
   git "$@" "${git_author}"
@@ -70,9 +75,9 @@ pair() {
     pair_commit $@
   else
     pair_reset
-    pair_configure 'author' $1
+    pair_configure 'pair.author' $1
     if [ -n "${2}" ]; then
-      pair_configure 'committer' $2
+      pair_configure 'pair.committer' $2
     fi
   fi
 }
